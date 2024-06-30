efb441db8366833bd560987e01c647662a1bbeb8
paologum <pgumasin@purdue.edu>
2024-06-30 02:57:42 -0400

    prettier changes

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -1,10 +1,10 @@
-import { extendTheme, type ThemeConfig } from '@chakra-ui/react'
+import { extendTheme, type ThemeConfig } from "@chakra-ui/react";
 
 const config: ThemeConfig = {
-  initialColorMode: 'system',
-  useSystemColorMode: false,
-}
+  initialColorMode: "system",
+  useSystemColorMode: false
+};
 
-const theme = extendTheme({ config })
+const theme = extendTheme({ config });
 
-export default theme
+export default theme;
