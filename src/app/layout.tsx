daabeea5f4c75094635eae9881cb03338d38e503
paologum <pgumasin@purdue.edu>
2024-06-30 03:06:20 -0400

    es lint support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ColorModeScript } from "@chakra-ui/react";
-import theme from "./theme";
+import { ColorModeScript, theme } from "@chakra-ui/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from 'next/image';
+import styles from './page.module.css';
 
 export default function Home() {
   return (
@@ -14,7 +14,7 @@ export default function Home() {
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer">
-            By{" "}
+            By{' '}
             <Image src="/vercel.svg" alt="Vercel Logo" className={styles.vercelLogo} width={100} height={24} priority />
           </a>
         </div>
