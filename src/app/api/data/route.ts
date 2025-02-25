1d2651c098df6d4ad508d1c374261a840adb0af4
paologum <pgumasin@purdue.edu>
2025-02-25 15:21:30 -0500

    add recipes

--- src/app/api/data/route.ts
+++ src/app/api/data/route.ts
@@ -1,7 +1,25 @@
-import { readFile } from "fs/promises";
+import { readFile, writeFile } from "fs/promises";
 
 export async function GET(req: Request) {
   const json = await readFile("Docs-utf8.json", "utf-8");
-  console.log(JSON.parse(json));
-  return Response.json(JSON.parse(json));
+  let final = "";
+  let recipes: { [str: string]: any } = {};
+  let altRecipes: { [str: string]: any } = {};
+
+  for (const obj of JSON.parse(json)) {
+    if (obj.NativeClass === "/Script/CoreUObject.Class'/Script/FactoryGame.FGRecipe'") {
+      final = JSON.stringify(obj, null, 2);
+      await writeFile("all-recipes.json", final, "utf-8");
+      for (const recipe of obj.Classes) {
+        if (recipe.ClassName.startsWith("Recipe_Alternate")) {
+          altRecipes[recipe.ClassName] = recipe;
+        } else {
+          recipes[recipe.ClassName] = recipe;
+        }
+      }
+    }
+  }
+  await writeFile("recipes.json", JSON.stringify(recipes, null, 2), "utf-8");
+  await writeFile("alt-recipes.json", JSON.stringify(altRecipes, null, 2), "utf-8");
+  return Response.json(final);
 }
