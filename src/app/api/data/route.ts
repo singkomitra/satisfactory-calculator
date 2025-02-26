d25316d95fb80404db70d2b3637643ea94122cf9
paologum <pgumasin@purdue.edu>
2025-02-26 01:51:18 -0500

    organized jsons

--- src/app/api/data/route.ts
+++ src/app/api/data/route.ts
@@ -1,6 +1,7 @@
 import { readFile } from "fs/promises";
+import { join } from "path";
 
 export async function GET(req: Request) {
-  const json = await readFile("parsed-typesafe-recipes.json", "utf-8");
+  const json = await readFile(join("src", "parsing", "jsons", "parsed-typesafe-recipes.json"), "utf-8");
   return Response.json(JSON.parse(json));
 }
